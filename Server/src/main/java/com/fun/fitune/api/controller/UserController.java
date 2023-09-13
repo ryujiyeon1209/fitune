4fb1ca9a63016137e947f479c39b4e35e695a4c3
subin <qotnqls77@naver.com>
2023-09-13 12:20:02 +0900

    Feat/BE : user controller 생성

--- Server/src/main/java/com/fun/fitune/api/controller/UserController.java
+++ Server/src/main/java/com/fun/fitune/api/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.fun.fitune.api.controller;
 
+import com.fun.fitune.api.dto.response.CommonResponse;
+import com.fun.fitune.api.dto.response.UserInfoResponse;
 import com.fun.fitune.api.service.UserService;
+import com.fun.fitune.db.domain.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +22,11 @@ public class UserController {
     private final UserService userService;
     final String SUCCESS = "SUCCESS";
 
+    //사용자 정보 조회
+    @GetMapping("/info/{userSeq}")
+    public ResponseEntity<CommonResponse<UserInfoResponse>> getUserInfo(@PathVariable("userSeq") int userSeq){
+        System.out.println("유저 정보 조회 ");
+        User userInfo = userService.getUserInfo(userSeq);
 
-    //사용자 세포이름 바꾸기
-
+    }
 }
