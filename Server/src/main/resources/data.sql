bc90b15a69d6e4dedc2b6889c2b37a58b198db28
zhddkfxks1@naver.com <zhddkfxks1@naver.com>
2023-09-21 14:09:44 +0900

    fix: data.sql 파일 수정

--- Server/src/main/resources/data.sql
+++ Server/src/main/resources/data.sql
@@ -1,11 +1,11 @@
 insert into user (age, bpm, email, height, nickname, weight) values(20, 100, "qotnqls1998@gmail.com", 165, "배수빈", 49);
 insert into user (age, bpm, email, height, nickname, weight) values(28, 70, "dudgjs304321@gmail.com", 173, "조영헌", 68);
 
-INSERT INTO `B306`.`exercise_list` (`exercise_name`) VALUES ('수영');
-INSERT INTO `B306`.`exercise_list` (`exercise_name`) VALUES ('등산');
-INSERT INTO `B306`.`exercise_list` (`exercise_name`) VALUES ('달리기');
+INSERT INTO `exercise_list` (`exercise_name`) VALUES ('수영');
+INSERT INTO `exercise_list` (`exercise_name`) VALUES ('등산');
+INSERT INTO `exercise_list` (`exercise_name`) VALUES ('달리기');
 
-INSERT INTO `B306`.`prefer_exercise` (`exercise_list_seq`, `user_seq`) VALUES ('1', '1');
-INSERT INTO `B306`.`prefer_exercise` (`exercise_list_seq`, `user_seq`) VALUES ('2', '1');
+INSERT INTO `prefer_exercise` (`exercise_list_seq`, `user_seq`) VALUES ('1', '1');
+INSERT INTO `prefer_exercise` (`exercise_list_seq`, `user_seq`) VALUES ('2', '1');
 
-INSERT INTO `B306`.`prefer_exercise` (`exercise_list_seq`, `user_seq`) VALUES ('1', '2');
+INSERT INTO `prefer_exercise` (`exercise_list_seq`, `user_seq`) VALUES ('1', '2');
