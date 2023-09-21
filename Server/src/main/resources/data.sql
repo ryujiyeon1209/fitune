dc8d7da6b6fdf4ae969fdd92376f6a38edc12131
zhddkfxks1@naver.com <zhddkfxks1@naver.com>
2023-09-22 08:51:24 +0900

    fix : data.sql 최종 수정

--- Server/src/main/resources/data.sql
+++ Server/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO `user` (age, bpm, email, height, nickname, weight) VALUES(20, 100, 'qotnqls1998@gmail.com', 165, '배수빈', 49);
-insert into `user` (age, bpm, email, height, nickname, weight) VALUES(28, 70, 'dudgjs304321@gmail.com', 173, '조영헌', 68);
+INSERT INTO `user` (age, bpm, email, height, nickname, weight) VALUES(28, 70, 'dudgjs304321@gmail.com', 173, '조영헌', 68);
 
 INSERT INTO `exercise_list` (`exercise_name`) VALUES ('수영');
 INSERT INTO `exercise_list` (`exercise_name`) VALUES ('등산');
