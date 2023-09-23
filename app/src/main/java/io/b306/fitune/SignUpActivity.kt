27b9afecbe240d5d225bef2b402cf5821442f65d
박태흠 <xomamoc@naver.com>
2023-09-24 05:17:03 +0900

    design : 회원가입 페이지 만듦

--- app/src/main/java/io/b306/fitune/SignUpActivity.kt
+++ app/src/main/java/io/b306/fitune/SignUpActivity.kt
@@ -2,10 +2,18 @@ package io.b306.fitune
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.b306.fitune.databinding.ActivityMainBinding
+import io.b306.fitune.databinding.ActivitySignUpBinding
+import io.b306.fitune.databinding.FragmentMainBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
