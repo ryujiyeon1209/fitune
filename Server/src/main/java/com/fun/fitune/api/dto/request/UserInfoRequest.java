532c82540f7dfd2002713bdde44d6ca06aac2f5d
조영헌 <dudgjs304321@google.com>
2023-09-27 13:09:42 +0900

    Fix/BE : userInfoRequest 수정

--- Server/src/main/java/com/fun/fitune/api/dto/request/UserInfoRequest.java
+++ Server/src/main/java/com/fun/fitune/api/dto/request/UserInfoRequest.java
@@ -1,5 +1,6 @@
 package com.fun.fitune.api.dto.request;
 
+import jakarta.persistence.Column;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,35 +8,7 @@ import lombok.Setter;
 @Setter
 public class UserInfoRequest {
     int userSeq;
-    private String email;
-    private String password;
-    private String nickName;
-    private int year;
+    private int age;
     private int height;
     private int weight;
-    private Integer bodyFatPer;
-    private int restingBpm;
-    private int activeBpm;
-    private int preferExerciseSeq;
-
-    public UserInfoRequest(String email, String password, String nickName, int year, int height, int weight, int bodyFatPer, int restingBpm, int activeBpm, int preferExerciseSeq) {
-        this.email = email;
-        this.password = password;
-        this.nickName = nickName;
-        this.year = year;
-        this.height = height;
-        this.weight = weight;
-        this.bodyFatPer = bodyFatPer;
-        this.restingBpm = restingBpm;
-        this.activeBpm = activeBpm;
-        this.preferExerciseSeq = preferExerciseSeq;
-    }
-
-    public UserInfoRequest(int userSeq, String nickName, int height, int weight, int bodyFatPer) {
-        this.userSeq = userSeq;
-        this.nickName = nickName;
-        this.height = height;
-        this.weight = weight;
-        this.bodyFatPer = bodyFatPer;
-    }
 }
