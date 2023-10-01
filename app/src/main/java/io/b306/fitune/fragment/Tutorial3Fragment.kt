7808240c1a8f14b3289b0405e43b1e65c6d3670e
zhddkfxks1@naver.com <zhddkfxks1@naver.com>
2023-10-01 13:56:50 +0900

    feat : 회원가입, 튜토리얼3 레이아웃 만들기

--- app/src/main/java/io/b306/fitune/fragment/Tutorial3Fragment.kt
+++ app/src/main/java/io/b306/fitune/fragment/Tutorial3Fragment.kt
@@ -2,10 +2,15 @@ package io.b306.fitune.fragment
 
 import android.content.Context
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import io.b306.fitune.R
 import io.b306.fitune.databinding.FragmentTutorial3Binding
 
 class Tutorial3Fragment : Fragment() {
@@ -39,8 +44,40 @@ class Tutorial3Fragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val btnTutorial3Start = view.findViewById<Button>(R.id.btn_tutorial3Start)
+        val remainingTimeTextView = view.findViewById<TextView>(R.id.remaining_time)
+        val progressBar = view.findViewById<ProgressBar>(R.id.progress_bar_timer)
+
+        var timer: CountDownTimer? = null
+        var totalTimeMillis: Long = 180000 // 3분 (60초 * 1000밀리초)
+
+        // 버튼 클릭 시 동작
+        btnTutorial3Start.setOnClickListener {
+
+            timer?.cancel()  // 타이머가 이미 작동 중인 경우 중지
+
+            // 타이머 초기화
+            timer = object : CountDownTimer(totalTimeMillis, 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    // 남은 시간 업데이트
+                    val secondsRemaining = millisUntilFinished / 1000
+                    remainingTimeTextView.text = "남은 시간: $secondsRemaining 초"
+
+                    // 프로그레스바 업데이트 (최대값: totalTimeMillis / 1000, 현재값: secondsRemaining)
+                    progressBar.progress = (totalTimeMillis / 1000 - secondsRemaining).toInt()
+                }
+
+                override fun onFinish() {
+                    remainingTimeTextView.text = "남은 시간: 0 초"
+                }
+            }
+
+            timer?.start()  // 타이머 시작
+        }
+
+
+        // 버튼이 클릭되면 다음 페이지로 이동
         binding.btnTutorial3.setOnClickListener{
-            // 버튼이 클릭되면 다음 페이지로 이동
             pageNavigator?.moveToNextPage()
         }
     }
