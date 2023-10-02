8927622f05add67df8370e71c685284e1ffbda46
zhddkfxks1@naver.com <zhddkfxks1@naver.com>
2023-10-02 21:24:27 +0900

    feat : 운동 누적 시간 로직 만들기

--- app/src/main/java/io/b306/fitune/fragment/ExerciseSelectedFragment.kt
+++ app/src/main/java/io/b306/fitune/fragment/ExerciseSelectedFragment.kt
@@ -1,11 +1,15 @@
 package io.b306.fitune.fragment
 
+import android.R
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import io.b306.fitune.databinding.FragmentExerciseSelectedBinding
+
 
 class ExerciseSelectedFragment : Fragment() {
 
@@ -24,6 +28,18 @@ class ExerciseSelectedFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // 로직 작성하는 곳
+        setContentView(R.layout.activity_exercise_selected)
+
+        // 버튼 초기화 및 클릭 리스너 설정
+        val startExerciseButton: Button = findViewById(R.id.btn_exerciseStart)
+        startExerciseButton.setOnClickListener { // ExerciseProgressActivity로 이동
+            val intent = Intent(this@ExerciseSelectedActivity, ExerciseProgressActivity::class.java)
+
+            // 현재 시간을 ExerciseProgressActivity로 전달
+            val startTime = System.currentTimeMillis()
+            intent.putExtra("startTime", startTime)
+            startActivity(intent)
+        }
     }
 
     companion object {
