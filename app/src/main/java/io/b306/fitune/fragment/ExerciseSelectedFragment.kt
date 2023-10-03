5e4e93cacf7b089d91908a70042ae0b89319043d
zhddkfxks1@naver.com <zhddkfxks1@naver.com>
2023-10-03 15:36:05 +0900

    fix : 누적운동시간 코드 수정

--- app/src/main/java/io/b306/fitune/fragment/ExerciseSelectedFragment.kt
+++ app/src/main/java/io/b306/fitune/fragment/ExerciseSelectedFragment.kt
@@ -6,12 +6,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
+import io.b306.fitune.activity.ExerciseProgressActivity
 import io.b306.fitune.databinding.FragmentExerciseSelectedBinding
 
 
-class ExerciseSelectedFragment : Fragment() {
+class ExerciseSelectedFragment : Fragment(), ExerciseProgressActivity.ExerciseSelectedListener {
 
     private var _binding: FragmentExerciseSelectedBinding? = null
     private val binding get() = _binding!!
@@ -26,23 +26,15 @@ class ExerciseSelectedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        // 로직 작성하는 곳
-        setContentView(R.layout.activity_exercise_selected)
-
-        // 버튼 초기화 및 클릭 리스너 설정
-        val startExerciseButton: Button = findViewById(R.id.btn_exerciseStart)
-        startExerciseButton.setOnClickListener { // ExerciseProgressActivity로 이동
-            val intent = Intent(this@ExerciseSelectedActivity, ExerciseProgressActivity::class.java)
-
-            // 현재 시간을 ExerciseProgressActivity로 전달
-            val startTime = System.currentTimeMillis()
-            intent.putExtra("startTime", startTime)
-            startActivity(intent)
-        }
     }
 
     companion object {
         fun newInstance() = ExerciseSelectedFragment()
+    }
+
+    override fun onExerciseStartButtonClicked() {
+        //ExerciseProgressActivity로 이동, 현재시간 주기!
+        val intent = Intent(activity, ExerciseProgressActivity::class.java)
+        startActivity(intent)
     }
 }
