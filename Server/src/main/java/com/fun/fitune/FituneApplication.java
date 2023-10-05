e14de0bd4680812e4edac5666254b09553e6d206
조영헌 <dudgjs304321@google.com>
2023-10-05 15:26:52 +0900

    Refactor/BE : main

--- Server/src/main/java/com/fun/fitune/FituneApplication.java
+++ Server/src/main/java/com/fun/fitune/FituneApplication.java
@@ -4,10 +4,4 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FituneApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(FituneApplication.class, args);
-    }
-
-}
+public class FituneApplication { public static void main(String[] args) { SpringApplication.run(FituneApplication.class, args); } }
